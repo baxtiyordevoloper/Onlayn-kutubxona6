3323bb0e38114c2bb6ee8df02f5ffbc1915e6039
Baxtiyor Shamshiddinov <baxtiyor.shamshiddinov@mail.ru>
2024-05-11 16:21:47 +0500

    create project

---
+++ src/App.vue
@@ -0,0 +1,200 @@
+<script setup>
+</script>
+
+<template>
+    <!-- container boshlandi -->
+    <div class="container-fluid">
+
+        <!-- header qatori boshlandi -->
+        <div class="row">
+            <div class="col">
+                <header>
+                    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+                        <div class="container-fluid">
+                            <a class="navbar-brand" href="index.html">Kitoblar</a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Link</a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Dropdown
+                                        </a>
+                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <li><a class="dropdown-item" href="#">Action</a></li>
+                                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                                            <li><hr class="dropdown-divider"></li>
+                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link disabled">Disabled</a>
+                                    </li>
+                                </ul>
+                                <form class="d-flex">
+                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                    <button class="btn btn-outline-success" type="submit">Search</button>
+                                </form>
+                            </div>
+                        </div>
+                    </nav>
+                </header>
+            </div>
+        </div>
+        <!-- header qatori tugadi -->
+
+        <!-- main qatori boshlandi -->
+        <div class="row mt-4">
+
+            <!-- category boshlandi -->
+            <div class="col-12 col-md-2">
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action">Detektiv</a>
+                    <a href="#" class="list-group-item list-group-item-action active" aria-current="true">Klassika</a>
+                    <a href="#" class="list-group-item list-group-item-action">Komediya</a>
+                    <a href="#" class="list-group-item list-group-item-action">Romantika</a>
+                    <a href="#" class="list-group-item list-group-item-action">Fantastika</a>
+                </div>
+            </div>
+            <!-- category tugadi -->
+
+            <!-- kontent boshlandi -->
+            <div class="col-12 col-md-10 mt-4 mt-md-0">
+
+                <!-- kitoblar qatori boshlandi -->
+                <div class="row">
+
+                    <!-- kitob boshlandi -->
+                    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
+                        <div class="card">
+                            <img src="@/assets/otkan%20kunlar.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">O'tkan kunlar</h5>
+                                <p class="card-text">Otabek va ikki qiz, yohud sevgi uchburchagi...</p>
+                                <a href="book.html" class="btn btn-primary">O'qish</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- kitob tugadi -->
+
+                    <!-- kitob boshlandi -->
+                    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
+                        <div class="card">
+                            <img src="@/assets/otkan%20kunlar.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">O'tkan kunlar</h5>
+                                <p class="card-text">Otabek va ikki qiz, yohud sevgi uchburchagi...</p>
+                                <a href="book.html" class="btn btn-primary">O'qish</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- kitob tugadi -->
+
+                    <!-- kitob boshlandi -->
+                    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
+                        <div class="card">
+                            <img src="@/assets/otkan%20kunlar.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">O'tkan kunlar</h5>
+                                <p class="card-text">Otabek va ikki qiz, yohud sevgi uchburchagi...</p>
+                                <a href="book.html" class="btn btn-primary">O'qish</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- kitob tugadi -->
+
+                    <!-- kitob boshlandi -->
+                    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
+                        <div class="card">
+                            <img src="@/assets/otkan%20kunlar.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">O'tkan kunlar</h5>
+                                <p class="card-text">Otabek va ikki qiz, yohud sevgi uchburchagi...</p>
+                                <a href="book.html" class="btn btn-primary">O'qish</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- kitob tugadi -->
+
+                    <!-- kitob boshlandi -->
+                    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
+                        <div class="card">
+                            <img src="@/assets/otkan%20kunlar.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">O'tkan kunlar</h5>
+                                <p class="card-text">Otabek va ikki qiz, yohud sevgi uchburchagi...</p>
+                                <a href="book.html" class="btn btn-primary">O'qish</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- kitob tugadi -->
+
+                    <!-- kitob boshlandi -->
+                    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
+                        <div class="card">
+                            <img src="@/assets/otkan%20kunlar.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">O'tkan kunlar</h5>
+                                <p class="card-text">Otabek va ikki qiz, yohud sevgi uchburchagi...</p>
+                                <a href="book.html" class="btn btn-primary">O'qish</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- kitob tugadi -->
+
+                </div>
+                <!-- kitoblar qatori tugadi -->
+
+                <!-- pagination qatori boshlandi -->
+                <div class="row">
+                    <div class="col">
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination">
+                                <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Previous">
+                                        <span aria-hidden="true">&laquo;</span>
+                                    </a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item active"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <!-- pagination qatori tugadi -->
+
+
+            </div>
+            <!-- kontent tugadi -->
+
+        </div>
+        <!-- main qatori tugadi -->
+
+        <!-- footer qatori boshlandi -->
+        <div class="row">
+            <div class="col">
+                <footer class="bg-dark text-light p-5">
+                    &copy; All rights reserved
+                </footer>
+            </div>
+        </div>
+        <!-- footer qatori tugadi -->
+
+    </div>
+    <!-- container tugadi -->
+</template>
+
+<style>
+
+
+</style>
