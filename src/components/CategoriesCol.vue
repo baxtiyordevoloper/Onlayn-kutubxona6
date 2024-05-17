a3877bdda03ceb38fa67b1b739e8156f7fbe75a1
Baxtiyor Shamshiddinov <baxtiyor.shamshiddinov@mail.ru>
2024-05-17 15:38:46 +0500

    create components

---
+++ src/components/CategoriesCol.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="col-12 col-md-2">
+        <div class="list-group">
+            <a href="#" class="list-group-item list-group-item-action">Detektiv</a>
+            <a href="#" class="list-group-item list-group-item-action active" aria-current="true">Klassika</a>
+            <a href="#" class="list-group-item list-group-item-action">Komediya</a>
+            <a href="#" class="list-group-item list-group-item-action">Romantika</a>
+            <a href="#" class="list-group-item list-group-item-action">Fantastika</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/FooterRow.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="row">
+        <div class="col">
+            <footer class="bg-dark text-light p-5">
+                &copy; All rights reserved
+            </footer>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
